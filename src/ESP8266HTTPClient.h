30d7a13283e3f993b5c0088e2f8cfb880794b4d0
Fabian Desoye <fabian.desoye@kabeldeutschland.de>
2016-07-11 15:43:09 +0200

    add HTTP PUT

--- src/ESP8266HTTPClient.h
+++ src/ESP8266HTTPClient.h
@@ -150,6 +150,8 @@ class HTTPClient {
         int GET();
         int POST(uint8_t * payload, size_t size);
         int POST(String payload);
+        int PUT(uint8_t * payload, size_t size);
+        int PUT(String payload);
         int sendRequest(const char * type, String payload);
         int sendRequest(const char * type, uint8_t * payload = NULL, size_t size = 0);
         int sendRequest(const char * type, Stream * stream, size_t size = 0);
